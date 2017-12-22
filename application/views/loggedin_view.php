e489248643b45a893b46f63c3818b26351762527
Rabin Shrestha <timeportal21@gmail.com>
2017-12-22 16:15:22 +0545

    edited line

--- application/views/loggedin_view.php
+++ application/views/loggedin_view.php
@@ -3,8 +3,7 @@
 	<p>Welcome First Name: <?php echo $this->session->userdata('loggedin')['firstname']; ?><br></p>
 	<a href="<?php echo base_url('home/signout'); ?>">Logout</a>
 <!--  -->
-	
-	
+
 <!--  -->
 <?php else: ?>
 	<?php redirect('home'); ?>
